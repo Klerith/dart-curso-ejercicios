1d450b35f40e6bcb6552980b85066619f290f989
Fernando Herrera <fernando.herrera85@gmail.com>
2019-07-09 22:09:52 -0400

    Fin secciíon 4

---
+++ 02_control/01_obtener_input.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+main() {
+
+  // imprimir en terminal o cmd
+  stdout.writeln('¿Cuál es tu nombre?');
+
+  // Leer información
+  String nombre = stdin.readLineSync(); // '10'
+
+  stdout.writeln('Tu nombre es: $nombre');
+
+  
+}
